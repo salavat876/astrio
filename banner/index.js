f9732dc404ffa248646bd9f7ffa2162ef0f2dfb5
Salavat <72986698+salavat876@users.noreply.github.com>
2021-07-14 11:57:34 +0400

    Add files via upload

---
+++ banner/index.js
@@ -0,0 +1,51 @@
+const dots = document.getElementsByClassName('dot'),
+        dotsArea = document.getElementById('dots-block'),
+        slides = document.getElementsByClassName('slider-item'),
+        prev = document.getElementById('prev-btn'),
+    next = document.getElementById('next-btn');
+
+
+let slideIndex = 1;
+
+
+const showSlides = (n) => {
+    if (n < 1) {
+        slideIndex = slides.length;
+    } else if (n > slides.length) {
+        slideIndex = 1;
+    }
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = 'none';
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove('dot-active');
+    }
+    slides[slideIndex - 1].style.display = 'block';
+    dots[slideIndex - 1].classList.add('dot-active');
+}
+
+showSlides(slideIndex);
+
+
+const plusSlides = (n) => {
+    showSlides(slideIndex += n);
+}
+
+const currentSlide = (n) => {
+    showSlides(slideIndex = n)
+}
+
+
+prev.onclick = function () {
+    plusSlides(-1);
+}
+next.onclick = function () {
+    plusSlides(1);
+}
+dotsArea.onclick = function (e) {
+    for (let i = 0; i < dots.length + 1; i++) {
+        if (e.target.classList.contains('dot') && e.target == dots[i - 1]) {
+            currentSlide(i);
+        }
+    }
+}
